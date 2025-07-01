4b4891aa369fbf91f0224887be7b86e9cc65133d
CasimiroSJ <casimirophiapo@gmail.com>
2025-07-01 21:28:14 +0100

    adicionais

--- site/src/pages/dashboard/Dashboard.js
+++ site/src/pages/dashboard/Dashboard.js
@@ -1,7 +1,24 @@
+import './styles.css'
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 
 function Dashboard(){
+    const [saire, setSair] = useState('')
+    const navigate = useNavigate();
+    function sair(){
+       setTimeout(() => {
+          navigate('/login');
+        }, 0);
+    }
         return(
-            <div>Seja-Vindo, Tropa...!</div>
+            <div className='dashboard'>
+                <h1>Seja-Vindo, Tropa...!</h1>
+
+                <button className='sair' onClick={sair}>Sair</button>
+
+            </div>
+
+        
         );   
 }
 export default Dashboard;
